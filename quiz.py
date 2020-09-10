658d4acc51300e177c44e11f65ad553baa297e70
Alexandra <sasa.matulova@gmail.com>
2020-09-10 12:56:35 +0200

    Adding SQLite database

--- quiz.py
+++ quiz.py
@@ -2,6 +2,8 @@ from flask import Flask, url_for, request
 from flask import render_template
 import random
 from random import randint
+import string
+import sqlite3
 
 
 app = Flask(__name__)
@@ -9,23 +11,70 @@ app.config['DEBUG'] = True
 print(app.root_path)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# SQLite execution
+def sql_execute(sql):
+    connection = sqlite3.connect('db.db')
+    cursor = connection.cursor()
+    ret = cursor.execute(sql)
+    connection.commit()
+    #connection.close()
+    return ret
 
+# Home page
 @app.route('/')
 def index():
-    return render_template('quiz_main.html')
+    session_id = get_random_string(32)
+    sql_execute(f"""
+        INSERT INTO USERS (SESSION_ID, NO_CORRECT_ANSWERS)
+        VALUES ('{session_id}', 0)
+    """)
+    return render_template('quiz_main.html', session_id=session_id)
 
-@app.route('/lets_start', methods=["GET", "POST"])
+
+# Question page
+@app.route('/question_page', methods=["GET", "POST"])
 def lets_start():
-    #question = random.choice(set_of_questions)
-    question_index = randint(0, len(set_of_questions)-1)
-    print(question_index)
-    return render_template('quiz.html', question=set_of_questions[question_index])
+    session_id = request.form['session_id']
 
+    # selecting question - excluding already asked questions
+    cursor_answered_questions = sql_execute(f"""
+        SELECT INDEX_ANSWERED_QUESTION FROM LIST_ANSWERED_QUESTIONS WHERE SESSION_ID = '{session_id}'
+    """)
+    answered_questions = cursor_answered_questions.fetchall()
+    answered_questions_res = [item for t in answered_questions for item in t]
+    question_index = randint(0, len(set_of_questions)-1)
+    while question_index in answered_questions_res:
+        question_index = randint(0, len(set_of_questions)-1)   
+        if len(answered_questions_res) == 3:
+            return render_template('quiz_result_page.html')
+    question = set_of_questions[question_index]
+    sql_execute(f"""
+        INSERT INTO LIST_ANSWERED_QUESTIONS (SESSION_ID, INDEX_ANSWERED_QUESTION)
+        VALUES ('{session_id}', {question_index})
+    """)
+    return render_template('quiz.html', question=question, question_index=question_index, session_id=session_id)
+
+
+# Answer evaluation page
 @app.route('/check_answer', methods=["GET", "POST"])
 def check_answer():
     users_choice = request.form['Options']
-    return render_template('quiz_evaluated_answer.html', users_choice=users_choice, question=question)
+    question_index = int(request.form['question_index'])
+    question = set_of_questions[question_index]
+    session_id = request.form['session_id']
+    if users_choice == question['correct_answer']:
+        cursor_no_correct_answers = sql_execute(f"""
+            SELECT NO_CORRECT_ANSWERS FROM USERS WHERE SESSION_ID='{session_id}'
+        """)
+        no_correct_answers = cursor_no_correct_answers.fetchone()[0]
+        no_correct_answers += 1
+        sql_execute(f"""
+            UPDATE USERS SET NO_CORRECT_ANSWERS={no_correct_answers} WHERE SESSION_ID='{session_id}'
+        """)
+    return render_template('quiz_evaluated_answer.html', users_choice=users_choice, question=question, session_id=session_id)
 
+
+# List of questions
 set_of_questions = [
     {
         "image": "images/allium_harry-grout-67I-shlEBLY-unsplash.jpg", 
@@ -47,6 +96,12 @@ set_of_questions = [
     }
 ]    
 
+
+# Session ID: Random string with the combination of lower and upper case, length 32
+def get_random_string(length):
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
 
 
 if __name__ == "__main__":
