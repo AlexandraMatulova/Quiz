70b06e18cd33a6f47f2c3fbdec6bb0a0922652e8
Alexandra <sasa.matulova@gmail.com>
2020-09-15 14:32:20 +0200

    Database added with set of questions

--- quiz.py
+++ quiz.py
@@ -3,66 +3,85 @@ from flask import render_template
 import random
 from random import randint
 import string
-import sqlite3
-
+from Question import Question
+from utils import sql_execute
 
 app = Flask(__name__)
 app.config['DEBUG'] = True 
 print(app.root_path)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# SQLite execution
-def sql_execute(sql):
-    connection = sqlite3.connect('db.db')
-    cursor = connection.cursor()
-    ret = cursor.execute(sql)
-    connection.commit()
-    #connection.close()
-    return ret
 
 # Home page
-@app.route('/')
+# generates a home page with a form to select a category of questions
+# after submitting the form the users is directed to 'question_page'
+@app.route('/', methods=["GET", "POST"])
 def index():
+    # session_id to distinguish between users, the value is passed from page to page
     session_id = get_random_string(32)
-    sql_execute(f"""
-        INSERT INTO USERS (SESSION_ID, NO_CORRECT_ANSWERS)
-        VALUES ('{session_id}', 0)
-    """)
-    return render_template('quiz_main.html', session_id=session_id)
+
+    # insert of session_id and no_correct_answers into 'users' table
+    sql_execute(f"INSERT INTO USERS (SESSION_ID, NO_CORRECT_ANSWERS) VALUES ('{session_id}', 0)")
+    return render_template('quiz_home_page.html', session_id=session_id)
 
 
-# Question page
+# Question page, redirect to Result page
 @app.route('/question_page', methods=["GET", "POST"])
 def lets_start():
+    # gets session_id from previous request
     session_id = request.form['session_id']
 
-    # selecting question - excluding already asked questions
+    # gets selected category of questions from home page or database
+    if 'category' in request.form:
+        category = request.form['category'] 
+        sql_execute(f"UPDATE USERS SET CATEGORY = '{category}' WHERE SESSION_ID = '{session_id}'")    
+    else:
+        cursor_category = sql_execute(f"SELECT CATEGORY FROM USERS WHERE SESSION_ID = '{session_id}'")
+        category = cursor_category.fetchone()[0]
+
+    # selects question - excludes already asked questions
     cursor_answered_questions = sql_execute(f"""
         SELECT INDEX_ANSWERED_QUESTION FROM LIST_ANSWERED_QUESTIONS WHERE SESSION_ID = '{session_id}'
     """)
     answered_questions = cursor_answered_questions.fetchall()
     answered_questions_res = [item for t in answered_questions for item in t]
-    question_index = randint(0, len(set_of_questions)-1)
-    while question_index in answered_questions_res:
-        question_index = randint(0, len(set_of_questions)-1)   
-        if len(answered_questions_res) == 3:
-            return render_template('quiz_result_page.html')
-    question = set_of_questions[question_index]
+
+    # gets list of IDs of questions from the chosen category
+    cursor_question_ids = sql_execute(f"SELECT ID FROM QUESTIONS WHERE CATEGORY = '{category}'")
+    question_ids = cursor_question_ids.fetchall()
+    question_ids_res = [item for t in question_ids for item in t]
+
+    #generates random question ID
+    question_id = question_ids_res[randint(0, len(question_ids_res)-1)]
+    while question_id in answered_questions_res:
+        question_id = question_ids_res[randint(0, len(question_ids_res)-1)]
+
+        #goes to result page when all questions of category answered
+        if len(answered_questions_res) == len(question_ids_res):
+            cursor_no_correct_answers = sql_execute(f"""
+                SELECT NO_CORRECT_ANSWERS FROM USERS WHERE SESSION_ID = '{session_id}'
+            """)
+            no_correct_answers = cursor_no_correct_answers.fetchone()[0]
+            no_questions = len(question_ids_res)
+            return render_template('quiz_result_page.html', no_correct_answers=no_correct_answers, no_questions=no_questions)
+    
     sql_execute(f"""
         INSERT INTO LIST_ANSWERED_QUESTIONS (SESSION_ID, INDEX_ANSWERED_QUESTION)
-        VALUES ('{session_id}', {question_index})
+        VALUES ('{session_id}', {question_id})
     """)
-    return render_template('quiz.html', question=question, question_index=question_index, session_id=session_id)
 
+    question = Question(question_id)
+    return render_template('quiz_question_page.html', question=question, session_id=session_id)
 
 # Answer evaluation page
 @app.route('/check_answer', methods=["GET", "POST"])
 def check_answer():
     users_choice = request.form['Options']
-    question_index = int(request.form['question_index'])
-    question = set_of_questions[question_index]
     session_id = request.form['session_id']
-    if users_choice == question['correct_answer']:
+    question = Question(int(request.form['question_index']))
+
+    # adds +1 to no of correct answeres in database
+    if question.is_correct_answer(users_choice):
         cursor_no_correct_answers = sql_execute(f"""
             SELECT NO_CORRECT_ANSWERS FROM USERS WHERE SESSION_ID='{session_id}'
         """)
@@ -72,29 +91,6 @@ def check_answer():
             UPDATE USERS SET NO_CORRECT_ANSWERS={no_correct_answers} WHERE SESSION_ID='{session_id}'
         """)
     return render_template('quiz_evaluated_answer.html', users_choice=users_choice, question=question, session_id=session_id)
-
-
-# List of questions
-set_of_questions = [
-    {
-        "image": "images/allium_harry-grout-67I-shlEBLY-unsplash.jpg", 
-        "question": "This flower is a genus of monocotyledonous flowering plants that includes hundreds of species, including the cultivated onion, garlic, scallion, shallot, leek, and chives. The generic name is a Latin word for garlic.",
-        "answers": ["Allium", "Rose", "Agapanthus"],
-        "correct_answer": "Allium"
-    },
-    {
-        "image": "images/clematis_dusan-smetana-haCOsgRt0vo-unsplash.jpg",
-        "question": "Their garden hybrids have been popular among gardeners,[7] beginning with Clematis × jackmanii, a garden standby since 1862; more hybrid cultivars are being produced constantly. They are mainly of Chinese and Japanese origin. Most species are known as clematis in English, while some are also known as traveller's joy, a name invented for the sole British native, C. vitalba, by the herbalist John Gerard; virgin's bower for C. terniflora, C. virginiana, and C. viticella; old man's beard, applied to several with prominent seedheads; leather flower for those with fleshy petals; or vase vine for the North American Clematis viorna.",
-        "answers": ["Hemerocallis", "Paeonia", "Clematis"],
-        "correct_answer": "Clematis"
-    },
-    {
-        "image": "images/hortensia_lucie-goncalves-RWcsCAKUMDA-unsplash.jpg",
-        "question": "It is a genus of 70–75 species of flowering plants native to Asia and the Americas. By far the greatest species diversity is in eastern Asia, notably Korea, China, and Japan. Most are shrubs 1 to 3 meters tall, but some are small trees, and others lianas reaching up to 30 m (98 ft) by climbing up trees. They can be either deciduous or evergreen, though the widely cultivated temperate species are all deciduous",
-        "answers": ["Hydrangea", "Lantana", "Kalmia"],
-        "correct_answer": "Hydrangea"
-    }
-]    
 
 
 # Session ID: Random string with the combination of lower and upper case, length 32
